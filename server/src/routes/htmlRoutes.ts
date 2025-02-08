d9f21ad0773ce1ba361bf0adaa9745ef816aa062
Lacy Ann Taylor <lacy.ann.taylor@gmail.com>
2025-02-07 22:22:01 -0600

    trying to fix API route

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -6,15 +6,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-// Route to serve index.html
-router.get('/', (_req: Request, res: Response) => {
-    const indexPath = path.join(__dirname, '../clinet/dist/index.html');
-    res.sendFile(indexPath);
+//Route for client side routing (e.g., React SPAs)
+router.get('*', (_req:Request, res:Response) => {
+    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
 });
-
-// Catch-all route to serve index.html for client-side routing (e.g., React SPAs)
-router.get('*', (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 export default router;
